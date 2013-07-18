47c8f64a30c45f514c5a6be9da73a22a81c77b74
numeral <numerical@gmail.com>
2013-07-18 12:40:33 +0100

    Add some state and minor fixes to function definitions

--- kobuki.h
+++ kobuki.h
@@ -1,21 +1,30 @@
 #ifndef KOBUKI_H
 #define KOBUKI_H
 
+#include <cstdio>
 #include <libplayercore/playercore.h>
+#include <kobuki_driver/kobuki.hpp>
 
 class Kobuki : public Driver {
     private:
+        position2d_id position_addr;
+        player_position2d_data data;
+
+        kobuki::Parameters parameters;
+        kobuki::Kobuki kobuki;
 
     public:
         Kobuki(ConfigFile*, int);
         ~Kobuki();
 
         virtual void Main();
-        virtual void Setup();
-        virtual void Shutdown();
+        virtual int Setup();
+        virtual int Shutdown();
 
         virtual int Subscribe(player_devaddr_t);
         virtual int Unsubscribe(player_devaddr_t);
-        virtual int ProcessMessage(MessageQueue, player_msghdr, void*)
+        virtual int ProcessMessage(MessageQueue*, player_msghdr*, void*);
+
+        virtual void PutData();
 };
 #endif
